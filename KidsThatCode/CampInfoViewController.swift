3ce480ecb4fc321db37dfc293a3270c2f9ca12ec
Hector Diaz <004854001@coyote.csusb.edu>
2018-07-08 19:25:19 -0700

    Finished the schedule function, minor bug where the user needs to go home to update then open schedule again

--- KidsThatCode/CampInfoViewController.swift
+++ KidsThatCode/CampInfoViewController.swift
@@ -2,40 +2,64 @@
 //  CampInfoViewController.swift
 //  KidsThatCode
 //
-//  Created by Hector Diaz on 7/1/18.
+//  Created by Hector Diaz on 7/8/18.
 //  Copyright © 2018 KidsThatCode. All rights reserved.
 //
 
 import UIKit
+import Firebase
 
-class CampInfoViewController: UIViewController, UITableViewDataSource{
-
-  
+class CampInfoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     @IBOutlet weak var scheduleTable: UITableView!
     
-    var fruits = ["Apple", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry", "Clementine", "Coconut", "Cranberry", "Fig", "Grape", "Grapefruit", "Kiwi fruit", "Lemon", "Lime", "Lychee", "Mandarine", "Mango", "Melon", "Nectarine", "Olive", "Orange", "Papaya", "Peach", "Pear", "Pineapple", "Raspberry", "Strawberry"]
+    var scheduleRef: DatabaseReference!
+    var scheduleArray: [String] = []
     
-    // MARK: - Table view data source
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fruits.count
+        return scheduleArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "schedule", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
         
-        let fruitName = fruits[indexPath.row]
-        cell.textLabel?.text = fruitName
+        let scheduleCell = scheduleArray[indexPath.row]
+        print(scheduleCell)
+        cell.textLabel?.text = scheduleCell
         return cell
-        
     }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        scheduleRef = Database.database().reference().child("Schedule").child("Camp")
+        scheduleRef.observe(DataEventType.value) { (snapshot) in
+            let snapshotValue = snapshot.value as? NSDictionary
+            let activity1 = snapshotValue!["Activity1"] as? String
+            self.scheduleArray.append(activity1!)
+            let activity2 = snapshotValue!["Activity2"] as? String
+            self.scheduleArray.append(activity2!)
+            let activity3 = snapshotValue!["Activity3"] as? String
+            self.scheduleArray.append(activity3!)
+            let activity4 = snapshotValue!["Activity4"] as? String
+            self.scheduleArray.append(activity4!)
+            let activity5 = snapshotValue!["Activity5"] as? String
+            self.scheduleArray.append(activity5!)
+            let activity6 = snapshotValue!["Activity6"] as? String
+            self.scheduleArray.append(activity6!)
+            let activity7 = snapshotValue!["Activity7"] as? String
+            self.scheduleArray.append(activity7!)
+            let activity8 = snapshotValue!["Activity8"] as? String
+            self.scheduleArray.append(activity8!)
+            
+            
+        }
+        self.scheduleTable.reloadData()
         // Do any additional setup after loading the view.
     }
 
